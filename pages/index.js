d121e7cc2c9fae2e0e1b999a77a2f510e77e3010
jordaogalheno <galheno93@gmail.com>
2025-01-13 11:57:36 +0000

    incluindo um emoji de oracao

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Direi do Senhor: Ele é o meu Deus, o meu refúgio, a minha fortaleza, e nele confiarei.</h1>
+    return <h1>Direi do Senhor: Ele é o meu Deus, o meu refúgio, a minha fortaleza, e nele confiarei. 🙏</h1>
 }
 
 export default Home;
