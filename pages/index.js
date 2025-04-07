04cd3ad5bc393ffe4a11af7f2c1f88fb58e26a41
jordaogalheno <galheno93@gmail.com>
2025-04-07 08:54:56 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Direi do Senhor: Ele é o meu Deus, o meu refúgio, a minha fortaleza, e nele confiarei. 🙏</h1>
+  return (
+    <h1>
+      Direi do Senhor: Ele é o meu Deus, o meu refúgio, a minha fortaleza, e
+      nele confiarei. 🙏
+    </h1>
+  );
+}
+
+function teste2() {
+  console.log("teste 2");
 }
 
 export default Home;
